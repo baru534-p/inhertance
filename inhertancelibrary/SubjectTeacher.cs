5ffeb8bc72d9ce47e5b58ed4ded7f42a351cd4fe
TavvaBhargavi <baru.tavva@gmail.com>
2023-11-01 11:14:14 +0530

    Add project files.

---
+++ inhertancelibrary/SubjectTeacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace inhertancelibrary
+{
+    public class SubjectTeacher : Teacher
+    {
+        private SubjectTeacher whichSubject;
+
+        public SubjectTeacher WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+
+
+        }
+    }
+}
